01409ed3053d9d5373d214cf2b34caded3793268
davidfeust <73194236+davidfeust@users.noreply.github.com>
2021-08-25 23:03:28 +0300

    percentage

--- frontend/src/components/MaxPercentage.js
+++ frontend/src/components/MaxPercentage.js
@@ -3,23 +3,23 @@ import '../styles/Max.css'
 
 class MaxPercentage extends React.Component {
 
+    range = (start, stop, step = 1) =>
+        Array(Math.ceil((stop + 1 - start) / step)).fill(start).map((x, y) => x + y * step);
+
     constructor(props) {
         super(props);
         this.state = {
-            value: "PPPercentage of summary!!"
+            value: ""
         };
+        this.percentage_list = this.range(10, 100, 5);
     }
 
     render() {
         return <div className="max-bg">
             <select className="max-input" style={{paddingTop: "13px"}} id="percentage-max"
-                    onChange={this.props.onChange1} required={this.props.required}
-            >
+                    onChange={this.props.onChange1} required={this.props.required}>
                 <option value="">Percentage of summary</option>
-                <option value="0.10">10%</option>
-                <option value="0.15">15%</option>
-                <option value="0.2">20%</option>
-                <option value="0.25">25%</option>
+                {this.percentage_list.map(x => <option value={x / 100} key={x}>{x}%</option>)}
             </select>
         </div>;
     }
